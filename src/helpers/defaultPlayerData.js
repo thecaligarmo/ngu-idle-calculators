2997f66f2d109d832e688eb04c74607b791f491f
Cali Garmo <aram.dermenjian.o@gmail.com>
2024-03-24 13:37:33 +0100

    Auto update numbers across the board

--- src/helpers/defaultPlayerData.js
+++ src/helpers/defaultPlayerData.js
@@ -1,95 +1,150 @@
 export function defaultPlayerData(playerData, info) {
     const playerExists = (playerData && Object.keys(playerData).length > 0)
-    switch(info) {
-        case 'baseEnergyPower':
-            return playerExists ? playerData.energyPower : 0;
-        case 'baseEnergyBar':
-            return playerExists ? playerData.energyBars.low : 0;
-        case 'baseEnergyCap':
-            return playerExists ? playerData.capEnergy.low : 0;
-        case 'baseMagicBar':
-            return playerExists ? playerData.magic.magicPerBar.low : 0;
-        case 'baseMagicCap':
-            return playerExists ? playerData.magic.capMagic.low : 0;
-        case 'baseMagicPower':
-            return playerExists ? playerData.magic.magicPower : 0;
-        case 'baseResource3Bar':
-            return playerExists ? playerData.res3.res3PerBar.low : 0;
-        case 'baseResource3Cap':
-            return playerExists ? playerData.res3.capRes3.low : 0;
-        case 'baseResource3Power':
-            return playerExists ? playerData.res3.res3Power : 0;
-        case 'resource3Active':
-            return playerExists ? playerData.res3.res3On : 0;
-        case 'currentEnergyCap':
-            return playerExists ? playerData.curEnergy.low : 0;
-        case 'currentMagicCap':
-            return playerExists ? playerData.magic.curMagic.low : 0;
-        case 'energyNGUAugmentsLevel':
-            return playerExists ? playerData.NGU.skills[0].level.low : 0;
-        case 'energyNGUWandoosLevel':
-            return playerExists ? playerData.NGU.skills[1].level.low : 0;
-        case 'energyNGURespawnLevel':
-            return playerExists ? playerData.NGU.skills[2].level.low : 0;
-        case 'energyNGUGoldLevel':
-            return playerExists ? playerData.NGU.skills[3].level.low : 0;
-        case 'energyNGUAdventureALevel':
-            return playerExists ? playerData.NGU.skills[4].level.low : 0;
-        case 'energyNGUPowerALevel':
-            return playerExists ? playerData.NGU.skills[5].level.low : 0;
-        case 'energyNGUDropChanceLevel':
-            return playerExists ? playerData.NGU.skills[6].level.low : 0;
-        case 'energyNGUMagicNGULevel':
-            return playerExists ? playerData.NGU.skills[7].level.low : 0;
-        case 'energyNGUPPLevel':
-            return playerExists ? playerData.NGU.skills[8].level.low : 0;
-        case 'magicNGUYggdrasilLevel':
-            return playerExists ? playerData.NGU.magicSkills[0].level.low : 0;
-        case 'magicNGUExpLevel':
-            return playerExists ? playerData.NGU.magicSkills[1].level.low : 0;
-        case 'magicNGUPowerBLevel':
-            return playerExists ? playerData.NGU.magicSkills[2].level.low : 0;
-        case 'magicNGUNumberLevel':
-            return playerExists ? playerData.NGU.magicSkills[3].level.low : 0;
-        case 'magicNGUTimeMachineLevel':
-            return playerExists ? playerData.NGU.magicSkills[4].level.low : 0;
-        case 'magicNGUEnergyNGULevel':
-            return playerExists ? playerData.NGU.magicSkills[5].level.low : 0;
-        case 'magicNGUAdventureBLevel':
-            return playerExists ? playerData.NGU.magicSkills[6].level.low : 0;
-        case 'energyNGUAugmentsTarget':
-            return playerExists ? playerData.NGU.skills[0].target.low : 0;
-        case 'energyNGUWandoosTarget':
-            return playerExists ? playerData.NGU.skills[1].target.low : 0;
-        case 'energyNGURespawnTarget':
-            return playerExists ? playerData.NGU.skills[2].target.low : 0;
-        case 'energyNGUGoldTarget':
-            return playerExists ? playerData.NGU.skills[3].target.low : 0;
-        case 'energyNGUAdventureATarget':
-            return playerExists ? playerData.NGU.skills[4].target.low : 0;
-        case 'energyNGUPowerATarget':
-            return playerExists ? playerData.NGU.skills[5].target.low : 0;
-        case 'energyNGUDropChanceTarget':
-            return playerExists ? playerData.NGU.skills[6].target.low : 0;
-        case 'energyNGUMagicNGUTarget':
-            return playerExists ? playerData.NGU.skills[7].target.low : 0;
-        case 'energyNGUPPTarget':
-            return playerExists ? playerData.NGU.skills[8].target.low : 0;
-        case 'magicNGUYggdrasilTarget':
-            return playerExists ? playerData.NGU.magicSkills[0].target.low : 0;
-        case 'magicNGUExpTarget':
-            return playerExists ? playerData.NGU.magicSkills[1].target.low : 0;
-        case 'magicNGUPowerBTarget':
-            return playerExists ? playerData.NGU.magicSkills[2].target.low : 0;
-        case 'magicNGUNumberTarget':
-            return playerExists ? playerData.NGU.magicSkills[3].target.low : 0;
-        case 'magicNGUTimeMachineTarget':
-            return playerExists ? playerData.NGU.magicSkills[4].target.low : 0;
-        case 'magicNGUEnergyNGUTarget':
-            return playerExists ? playerData.NGU.magicSkills[5].target.low : 0;
-        case 'magicNGUAdventureBTarget':
-            return playerExists ? playerData.NGU.magicSkills[6].target.low : 0;
-        default:
-            return 0;
+    if (playerExists) {
+        switch(info) {
+            case 'baseEnergyPower':
+                return playerData.energyPower;
+            case 'baseEnergyBar':
+                return playerData.energyBars.low;
+            case 'baseEnergyCap':
+                return playerData.capEnergy.low;
+            case 'baseMagicBar':
+                return playerData.magic.magicPerBar.low;
+            case 'baseMagicCap':
+                return playerData.magic.capMagic.low;
+            case 'baseMagicPower':
+                return playerData.magic.magicPower;
+            case 'baseResource3Bar':
+                return playerData.res3.res3PerBar.low;
+            case 'baseResource3Cap':
+                return playerData.res3.capRes3.low;
+            case 'baseResource3Power':
+                return playerData.res3.res3Power;
+
+            case 'currentEnergyCap':
+                return playerData.curEnergy.low;
+            case 'currentMagicCap':
+                return playerData.magic.curMagic.low;
+
+            case 'energyNGUAugmentsLevel':
+                return playerData.NGU.skills[0].level.low;
+            case 'energyNGUWandoosLevel':
+                return playerData.NGU.skills[1].level.low;
+            case 'energyNGURespawnLevel':
+                return playerData.NGU.skills[2].level.low;
+            case 'energyNGUGoldLevel':
+                return playerData.NGU.skills[3].level.low;
+            case 'energyNGUAdventureALevel':
+                return playerData.NGU.skills[4].level.low;
+            case 'energyNGUPowerALevel':
+                return playerData.NGU.skills[5].level.low;
+            case 'energyNGUDropChanceLevel':
+                return playerData.NGU.skills[6].level.low;
+            case 'energyNGUMagicNGULevel':
+                return playerData.NGU.skills[7].level.low;
+            case 'energyNGUPPLevel':
+                return playerData.NGU.skills[8].level.low;
+            case 'energyNGUAugmentsTarget':
+                return playerData.NGU.skills[0].target.low;
+            case 'energyNGUWandoosTarget':
+                return playerData.NGU.skills[1].target.low;
+            case 'energyNGURespawnTarget':
+                return playerData.NGU.skills[2].target.low;
+            case 'energyNGUGoldTarget':
+                return playerData.NGU.skills[3].target.low;
+            case 'energyNGUAdventureATarget':
+                return playerData.NGU.skills[4].target.low;
+            case 'energyNGUPowerATarget':
+                return playerData.NGU.skills[5].target.low;
+            case 'energyNGUDropChanceTarget':
+                return playerData.NGU.skills[6].target.low;
+            case 'energyNGUMagicNGUTarget':
+                return playerData.NGU.skills[7].target.low;
+            case 'energyNGUPPTarget':
+                return playerData.NGU.skills[8].target.low;
+                
+            case 'magicNGUYggdrasilLevel':
+                return playerData.NGU.magicSkills[0].level.low;
+            case 'magicNGUExpLevel':
+                return playerData.NGU.magicSkills[1].level.low;
+            case 'magicNGUPowerBLevel':
+                return playerData.NGU.magicSkills[2].level.low;
+            case 'magicNGUNumberLevel':
+                return playerData.NGU.magicSkills[3].level.low;
+            case 'magicNGUTimeMachineLevel':
+                return playerData.NGU.magicSkills[4].level.low;
+            case 'magicNGUEnergyNGULevel':
+                return playerData.NGU.magicSkills[5].level.low;
+            case 'magicNGUAdventureBLevel':
+                return playerData.NGU.magicSkills[6].level.low;
+            case 'magicNGUYggdrasilTarget':
+                return playerData.NGU.magicSkills[0].target.low;
+            case 'magicNGUExpTarget':
+                return playerData.NGU.magicSkills[1].target.low;
+            case 'magicNGUPowerBTarget':
+                return playerData.NGU.magicSkills[2].target.low;
+            case 'magicNGUNumberTarget':
+                return playerData.NGU.magicSkills[3].target.low;
+            case 'magicNGUTimeMachineTarget':
+                return playerData.NGU.magicSkills[4].target.low;
+            case 'magicNGUEnergyNGUTarget':
+                return playerData.NGU.magicSkills[5].target.low;
+            case 'magicNGUAdventureBTarget':
+                return playerData.NGU.magicSkills[6].target.low;
+            case 'resource3Active':
+                return playerData.res3.res3On;
+            default:
+                return 0;
+        }
     }
+    return 0;
+}
+
+export function getPlayerOptions() {
+    return [
+        'baseEnergyPower',
+        'baseEnergyBar',
+        'baseEnergyCap',
+        'baseMagicBar',
+        'baseMagicCap',
+        'baseMagicPower',
+        'baseResource3Bar',
+        'baseResource3Cap',
+        'baseResource3Power',
+        'currentEnergyCap',
+        'currentMagicCap',
+        'energyNGUAugmentsLevel',
+        'energyNGUWandoosLevel',
+        'energyNGURespawnLevel',
+        'energyNGUGoldLevel',
+        'energyNGUAdventureALevel',
+        'energyNGUPowerALevel',
+        'energyNGUDropChanceLevel',
+        'energyNGUMagicNGULevel',
+        'energyNGUPPLevel',
+        'energyNGUAugmentsTarget',
+        'energyNGUWandoosTarget',
+        'energyNGURespawnTarget',
+        'energyNGUGoldTarget',
+        'energyNGUAdventureATarget',
+        'energyNGUPowerATarget',
+        'energyNGUDropChanceTarget',
+        'energyNGUMagicNGUTarget',
+        'energyNGUPPTarget',
+        'magicNGUYggdrasilLevel',
+        'magicNGUExpLevel',
+        'magicNGUPowerBLevel',
+        'magicNGUNumberLevel',
+        'magicNGUTimeMachineLevel',
+        'magicNGUEnergyNGULevel',
+        'magicNGUAdventureBLevel',
+        'magicNGUYggdrasilTarget',
+        'magicNGUExpTarget',
+        'magicNGUPowerBTarget',
+        'magicNGUNumberTarget',
+        'magicNGUTimeMachineTarget',
+        'magicNGUEnergyNGUTarget',
+        'magicNGUAdventureBTarget',
+        'resource3Active',
+    ]
 }
--- src/helpers/stateForData.js
+++ src/helpers/stateForData.js
@@ -1,15 +1,18 @@
-import { defaultPlayerData } from "./defaultPlayerData";
+import { defaultPlayerData, getPlayerOptions } from "./defaultPlayerData";
 import { getPlayerData, isPlayerDataUpdated } from "./context";
 import { useLocalStorageNumber } from "./localStorage";
 
 export default function createStatesForData(data) {
     const playerData = getPlayerData();
+    var playerOptions = getPlayerOptions();
+    var receivedKeys = []
 
     var ir = []
     var dataObj = {}
     for (var col of data) {
         let colDr = []
         for (var k of col) {
+            receivedKeys.push(k)
             var defaultVal = defaultPlayerData(playerData, k)
             var dataState = useLocalStorageNumber(k, defaultVal)
             if (isPlayerDataUpdated() && dataState[0] != defaultVal) {
@@ -24,5 +27,10 @@ export default function createStatesForData(data) {
         }
         ir.push(colDr)
     }
+    playerOptions.map((opt) => {
+        if (!(opt in receivedKeys)) {
+            useLocalStorageNumber(opt, 0)
+        }
+    })
     return [ir, dataObj]
 }
