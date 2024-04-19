ac7c72142dec73e52cd8397dac3960451041e587
Cali Garmo <aram.dermenjian.o@gmail.com>
2024-04-18 17:35:57 -0700

    Add attribution to farmer Sanc

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -40,6 +40,7 @@ export default function Page() {
                         <li key="gearOptimizer"><Link href="https://gmiclotte.github.io/gear-optimizer/">Gear Optimizer</Link> (Importing Save File)</li>
                         <li key="simpleRatioCal"><Link href="https://docs.google.com/spreadsheets/d/1gy4GtvZZlNQG_uWd-DWVvl2hBi6Ha18A9ojFgXevuLA/">NGU - Simple ratio calculator by ily</Link> (Ratios)</li>
                         <li key="expPPNguCal"><Link href="https://docs.google.com/spreadsheets/d/1_tR_k-X32DPe5sd1ACKfHmY6uuiqDOYNsSxxb0EqZOo/">The Bestest MostPerfectest Exp, PP and NGU Calculator</Link> (NGUs)</li>
+                        <li key="farmerSancBoostAlmanac"><Link href="https://docs.google.com/spreadsheets/d/1UyOPvZ_Gen02xfJZuPGnOQNVETxoQXlYJ9ObHmmDWRI/edit#gid=0">Farmer Sanc's Boost Almanac</Link> (boost / exp) - Last updated 11/29/20 - Discord: Sancdar#0032</li>
                     </ul>
                 </>
             </ContentSection>
