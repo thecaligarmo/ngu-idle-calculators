29f81ca742da3374e605ca56cbc49fde5f7b361c
Cali Garmo <aram.dermenjian.o@gmail.com>
2024-05-15 12:18:53 -0700

    Remove unnecessary components - with error

--- src/components/timeText.tsx
+++
@@ -1,7 +0,0 @@
-export default function TimeText() {
-    return (
-        <p className="mb-2 text-sm">
-        The time formats are given in D:H:M:S where D stands for days, H for hours, M for minutes, S for seconds. For example: 2:04:16:12 means it will take 2 days, 4 hours, 16 minutes and 12 seconds.
-        </p>
-    );
-  }
